cbb028016637a60c6801a4fe3f6cc8db0a7d7158
Conor Odell <conorao@users.noreply.github.com>
2016-02-16 18:39:59 -0500

    bad, but works

--- url-generator.rb
+++ url-generator.rb
@@ -1,11 +1,33 @@
 require 'sinatra'
+require 'pry'
 
-get '/list/:dir' do
-  list_files(params[:dir])
+get '/list' do
+  prefix = params[:prefix]
+  dir = params[:dir]
+  filetype = params[:filetype]
+
+  unless params[:play_command].nil?
+    play_command = to_bool(params[:play_command])
+  end
+
+  list_files(prefix, dir, filetype, play_command)
 end
 
-def list_files(dir)
-  files = Dir["/var/www/kw/*"]
-  files2 = Dir.entries("/var/www/kw/thelifeofpablo")
-  puts files2
+def list_files(prefix, dir, filetype, play_command = false)
+  # move to the dir passed in
+  Dir.chdir(dir)
+
+  puts play_command
+  # Get all files of type filetype
+  files = Dir.glob("**/*.#{filetype}")
+
+  if play_command
+    output = files.map{ |file| "!play #{prefix}/#{file}"}.join("<br>")
+  else
+    output = files.map{ |file| "#{prefix}/#{file}" }
+  end
+end
+
+def to_bool(str)
+  str.downcase == 'true' || str = '1'
 end
