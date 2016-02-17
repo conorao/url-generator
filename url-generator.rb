9f35609e68d972257295143f2c87c50ecaa6f450
Conor Odell (server) <conorodell@conorodell.com>
2016-02-16 20:04:28 -0500

    works

--- url-generator.rb
+++ url-generator.rb
@@ -1,38 +1,37 @@
 require 'sinatra'
 require 'pry'
+require 'uri'
 
-class UrlGenerator < Sinatra::Base
-  set :run, true
-  set :bind, '0.0.0.0'
+set :run, true
+set :bind, '0.0.0.0'
 
-  get '/list' do
-    prefix = params[:prefix]
-    dir = params[:dir]
-    filetype = params[:filetype]
+get '/list' do
+  prefix = params[:prefix]
+  dir = params[:dir]
+  filetype = params[:filetype]
 
-    unless params[:play_command].nil?
-      play_command = to_bool(params[:play_command])
-    end
-
-    list_files(prefix, dir, filetype, play_command)
+  unless params[:play_command].nil?
+    play_command = to_bool(params[:play_command])
   end
 
-  def list_files(prefix, dir, filetype, play_command = false)
-    # move to the dir passed in
-    Dir.chdir(dir)
+  list_files(prefix, dir, filetype, play_command)
+end
 
-    puts play_command
-    # Get all files of type filetype
-    files = Dir.glob("**/*.#{filetype}")
+def list_files(prefix, dir, filetype, play_command = false)
+  # move to the dir passed in
+  Dir.chdir(dir)
 
-    if play_command
-      output = files.map{ |file| "!play #{prefix}/#{file}"}.join("<br>")
-    else
-      output = files.map{ |file| "#{prefix}/#{file}" }
-    end
+  puts play_command
+  # Get all files of type filetype
+  files = Dir.glob("**/*.#{filetype}")
+
+  if play_command
+    output = files.map{ |file| "!play #{prefix}/#{file}"}.join("<br>")
+  else
+    output = files.map{ |file| URI.escape("#{prefix}/#{file}") }.join("<br>")
   end
+end
 
-  def to_bool(str)
-    str.downcase == 'true' || str = '1'
-  end
+def to_bool(str)
+  str.downcase == 'true' || str = '1'
 end
